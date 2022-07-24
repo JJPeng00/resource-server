5d23f5998fde5aaeb46f3e7a788cd27e86d68d83
JJPeng <pengjiajun755@163.com>
2022-07-24 17:37:05 +0800

    引入依赖，创建主启动类

---
+++ src/main/java/com/jjpeng/resourceserver/Application.java
@@ -0,0 +1,15 @@
+package com.jjpeng.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JJPeng
+ * @date 2022/7/24 17:35
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
